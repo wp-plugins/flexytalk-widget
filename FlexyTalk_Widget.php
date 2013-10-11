6f181110116995bc0424cd0a14ed17ae92824c46
sodena <sodena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-11 16:54:03 +0000

    bug fix
    git-svn-id: https://plugins.svn.wordpress.org/flexytalk-widget/tags/3.1@786513 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- FlexyTalk_Widget.php
+++ FlexyTalk_Widget.php
@@ -22,7 +22,7 @@ function Get_Code() {
 $widget_id=get_option(ft_widget_id);
 $htmlCode="<script id='ftcontent' data-flexytalk=".$widget_id.">
          var script = document.createElement('script');
-         script.src = ('https' == document.location.protocol ? 'https:' : 'http:') + '//www.flexytalk.net/app/v3/js/flexytalk.js';
+         script.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + '//www.flexytalk.net/app/v3/js/flexytalk.js';
       
          document.getElementsByTagName('head')[0].appendChild(script);
    </script>";
